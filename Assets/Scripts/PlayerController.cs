93916d0d6cfcc501c2879522ab9187dd691c8f11
IsaacAndra <isaac.andra84@gmail.com>
2022-08-02 11:09:01 -0300

    Player Input Implemented

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //creating we atributes.
+    private Rigidbody2D rb;
+
+    [SerializeField] private float velocity = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Getting my RigidBody
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input Impulse
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //making the impulse velocity from rb;
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+}
